c65806994455ac3265ad2a1f80b6465ae575ca97
Annie Zhang <zhang.annie1@northeastern.edu>
2024-12-06 03:12:02 -0500

    post modal

--- Posts/schema.js
+++ Posts/schema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     user: String,
-    title: String,
     location: String,
     description: String,
     climbType: {
@@ -9,7 +8,6 @@ const postSchema = new mongoose.Schema({
         enum: ["Overhang", "Slab", "Cave"]
     },
     angle: Number,
-    grade: String,
     photo: String,
     likes: Array,
     }, {collection: "posts"}
